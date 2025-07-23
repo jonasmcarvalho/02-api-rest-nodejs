2b47019b4d53ce91f06e212394097c6f521e19c6
Jonas Carvalho <jonasmcarvalho@outlook.com>
2025-07-23 08:16:05 -0300

    Test render 1.2

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ import { app } from './app'
 app
   .listen({
     port: env.PORT,
-    host: 'render' in process.env ? '0.0.0.0' : 'localhost',
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP Server Running!')
